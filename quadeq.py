42bd3c450ac3b5b3b9f1b8dc169d34d1c5e75a0e
geoff <gaford@loyola.edu>
2018-09-24 16:38:17 -0400

    solving the quad eq.

---
+++ quadeq.py
@@ -0,0 +1,19 @@
+#program that solves the quadratic equations
+
+import cmath
+
+#what are the variables
+a = float(input("what is your A value:"))
+b = float(input("What is your B value:"))
+c = float(input("What is your C value:"))
+
+#calculate
+disc = (b**2) - (4*a*c)
+
+x_val1 = (-b + cmath.sqrt(disc))//(2*a)
+
+x_val2 = (-b - cmath.sqrt(disc))//(2*a)
+
+print ("x = ", x_val1)
+
+print ("x = ", x_val2)
